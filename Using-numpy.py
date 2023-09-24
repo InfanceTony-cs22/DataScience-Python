3b396288f4697f0c8c90986e4e5ae5efeaf7a5f9
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-24 20:29:11 +0530

    Using-numpy.py

---
+++ Using-numpy.py
@@ -0,0 +1,20 @@
+#unsing numpy library
+import numpy as np
+
+# Create a NumPy array from a list of numbers
+my_array = np.array([1, 2, 3, 4, 5])
+
+# Display the array
+print("NumPy Array:")
+print(my_array)
+
+# Calculate the mean of the elements
+mean_value = np.mean(my_array)
+
+# Multiply all elements by 2
+doubled_array = my_array * 2
+
+# Display the results
+print("\nMean Value:", mean_value)
+print("\nDoubled Array:")
+print(doubled_array)
