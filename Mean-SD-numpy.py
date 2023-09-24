1095c6d24a4e36adea4735e54df4b5b484a4164f
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-24 20:47:13 +0530

    Mean-SD-numpy.py

---
+++ Mean-SD-numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Create an array of test scores
+scores = np.array([85, 90, 78, 92, 88])
+
+# Calculate the mean and standard deviation
+mean_score = np.mean(scores)
+std_deviation = np.std(scores)
+
+# Display the results
+print("Mean Score:", mean_score)
+print("Standard Deviation:", std_deviation)
