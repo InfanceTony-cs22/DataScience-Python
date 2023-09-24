12b97b81d6d8a7b56a5703f309230c904c7568ce
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-24 20:41:46 +0530

    Matrix-Multiplicatio-numpy.py

---
+++ Matrix Multiplicatio-numpy.py
@@ -0,0 +1,16 @@
+
+#2X2 Matrix
+
+
+import numpy as np
+
+# Create two matrices
+matrix1 = np.array([[1, 2], [3, 4]])
+matrix2 = np.array([[5, 6], [7, 8]])
+
+# Multiply the matrices
+result_matrix = np.dot(matrix1, matrix2)
+
+# Display the result
+print("Result Matrix:")
+print(result_matrix)
