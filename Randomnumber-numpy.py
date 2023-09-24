e5e0760edd078cd6971650efaa3160c5fb7bf7f8
Infance Tony.E <138364215+InfanceTony-cs22@users.noreply.github.com>
2023-09-24 20:40:25 +0530

    Random-number-numpy.py

---
+++ Randomnumber-numpy.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+# Generate an array of 5 random integers between 1 and 10
+random_numbers = np.random.randint(1, 11, 5)
+
+# Display the random numbers
+print("Random Numbers:", random_numbers)
